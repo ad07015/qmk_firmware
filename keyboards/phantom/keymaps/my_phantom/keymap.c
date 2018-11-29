21aca522e54ad2d9a0522273bf4e27b4b60f3986
andrejs.dasko <andrejs.dasko@accenture.com>
2018-11-29 14:59:47 +0200

    Removed unnecessary mappings, added media control and numpad digit keys

--- keyboards/phantom/keymaps/my_phantom/keymap.c
+++ keyboards/phantom/keymaps/my_phantom/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2017 Mathias Andersson <wraul@dbox.se>
+/* Copyright 2018 Andrejs Dasko <andrejs.dasko@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,31 +17,31 @@
 
 // Helpful defines
 #define _______ KC_TRNS
+#define XXXXXXX KC_NO
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
+// _______ mean 'use the same button as the base layer'
+// XXXXXXX will make the button have no affect on the respective layer
 #define _BL 0
 #define _FL 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT_tkl_ansi(
+    [_BL] = LAYOUT_tkl_ansi_plus_five(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       KC_PSCR, KC_SLCK, KC_BRK,  \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_INS,  KC_HOME, KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_DEL,  KC_END,  KC_PGDN, \
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                  \
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,               KC_UP,            \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(_FL), KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT  \
+        MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,       KC_MPRV, KC_MPLY, KC_MNXT, \
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,      KC_VOLD, KC_UP,   KC_VOLU, \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_CAPS, KC_APP,  KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
-    [_FL] = LAYOUT_tkl_ansi(
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT,      KC_MUTE, KC_VOLD, KC_VOLU, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MSEL,                                 \
-        _______,          _______, _______, KC_CALC, _______, _______, _______, _______, _______, _______, _______,          _______,               _______,          \
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______,      _______, _______, _______  \
-    ),
+    [_FL] = LAYOUT_tkl_ansi_plus_five(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,       KC_7,    KC_8,    KC_9,    \
+        _______, KC_WH_U, KC_UP,   KC_WH_D, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,      KC_4,    KC_5,    KC_6,    \
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN,          _______,      KC_1,    KC_2,    KC_3,    \
+        _______,          _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, _______, KC_INS,  KC_DEL,           _______,      KC_0,    KC_0,    KC_DOT,  \
+        _______, _______, _______,                            RESET,                              _______, _______, _______, _______,      _______, _______, _______  \
+    )
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
