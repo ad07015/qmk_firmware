1958e4d040c728c4d45e3628cd446b10dfb599e1
andrejs.dasko <andrejs.dasko@gmail.com>
2020-02-04 09:22:49 +0200

    Fixed alignment

--- keyboards/dz60/keymaps/my_dz60/keymap.c
+++ keyboards/dz60/keymaps/my_dz60/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_WH_U, KC_UP,   KC_WH_D, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_PAUS, KC_HOME, KC_END,  KC_PSCR,  
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGUP, KC_PGDN,          _______,
         _______,          _______, _______, KC_CALC, _______, _______, _______, KC_WH_D, KC_WH_U, KC_INS,  KC_DEL,           _______,
-        _______, _______,  _______,                           _______,                            _______, _______, _______, _______),
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______),
 
     LAYOUT_60_ansi(
-        KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_DEL,
+        KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______,          _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, BL_BRTG, _______, _______, _______,          _______,
