29001dea8562d8e6f3bf316af1d9cb5fc7ad3602
andrejs.dasko <andrejs.dasko@gmail.com>
2020-02-15 14:20:09 +0200

    Refreshed my phantom layout based on my latest DZ60 layout

--- keyboards/phantom/keymaps/my_phantom/keymap.c
+++ keyboards/phantom/keymaps/my_phantom/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 Andrejs Dasko <andrejs.dasko@gmail.com>
+/* Copyright 2020 Andrejs Dasko <andrejs.dasko@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7,85 +7,63 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To flash this layout run the following command:
+ * make phantom:my_phantom:teensy
  */
 #include QMK_KEYBOARD_H
 
 // Helpful defines
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+#define FN_APP  LT(2, KC_APP)
+#define TO_BASE TO(0)
+#define TO_GAME TO(3)
 
 // Macros
 enum custom_keycodes {
-	M_EMAIL = SAFE_RANGE
+    M_EMAIL = SAFE_RANGE
 };
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// _______ mean 'use the same button as the base layer'
-// XXXXXXX will make the button have no affect on the respective layer
-#define _BL 0
-#define _FL 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT_tkl_ansi_plus_five(
-        KC_ESC,           		   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    	KC_PSCR, KC_SLCK, KC_BRK, 
+    LAYOUT_tkl_ansi_plus_five(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_BRK,
 
-        KC_GRV,  		  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_INS,  KC_HOME, KC_PGUP,
-        KC_TAB,  		  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_DEL,  KC_END,  KC_PGDN,
-        LT(_FL, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,       KC_MPRV, KC_MPLY, KC_MNXT,
-        KC_LSFT,          		   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,      KC_VOLD, KC_UP,   KC_VOLU,
-        KC_LCTL, KC_LGUI, KC_LALT,                            		   KC_SPC,                             KC_RALT, KC_CAPS, KC_APP,  KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT 
-    ),
-    [_FL] = LAYOUT_tkl_ansi_plus_five(
-        _______,          		   M_EMAIL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      KC_PAST, KC_PSLS, _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,
+        MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,     KC_MPRV, KC_MPLY, KC_MNXT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,    KC_VOLD, KC_UP,   KC_VOLU,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, FN_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT),
 
-        _______, 		  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,       KC_PPLS, KC_PMNS, KC_EQL,
-        _______, 		  KC_WH_U, KC_UP,   KC_WH_D, _______, _______, _______, _______, KC_UP,   _______, KC_PAUS, KC_HOME, KC_END,  KC_PSCR,      KC_7,    KC_8,    KC_9,   
-        _______,		  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,          _______,      KC_4,    KC_5,    KC_6,   
-        _______,          		   _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, _______, KC_INS,  KC_DEL,           _______,      KC_1,    KC_2,    KC_3,   
-        _______, _______, _______,                            RESET,                            		   _______, _______, _______, _______,      KC_0,    KC_DOT,  KC_PENT
-    )
+    LAYOUT_tkl_ansi_plus_five(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,     _______, _______, _______,
+        _______, KC_WH_U, KC_UP,   KC_WH_D, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_PAUS, KC_HOME, KC_END,  KC_PSCR,    _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGUP, KC_PGDN,          _______,    _______, _______, _______,
+        _______,          _______, _______, KC_CALC, _______, _______, _______, KC_WH_D, KC_WH_U, KC_INS,  KC_DEL,           _______,    _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______,    _______, _______, _______),
+
+    LAYOUT_tkl_ansi_plus_five(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+
+        KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______,    _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,      _______, _______, _______,
+        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,    _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,    _______, _______, _______,
+        _______, TO_GAME,          _______,                   _______,                            _______, _______, _______, TO_BASE,    _______, _______, _______),
+
+    LAYOUT_tkl_ansi_plus_five(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,    _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,    _______, _______, _______,
+        _______, XXXXXXX,          _______,                   _______,                            _______, XXXXXXX, _______, _______,    _______, _______, _______)
 };
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-    // MACRODOWN only works in this function
-    switch(id) {
-        case 0:
-            if (record->event.pressed) {
-                register_code(KC_RSFT);
-            } else {
-                unregister_code(KC_RSFT);
-            }
-            break;
-    }
-    return MACRO_NONE;
-};
-
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch(keycode) {
-        	case M_EMAIL:
-        		SEND_STRING("andrejs.dasko@gmail.com");
-        		return false; break;
-        }
-    }
-    return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
